7f311bc3e359757833566874376d1e3cdda1077f
Алёна Кузьмина <Soboleva-Aloyna@bk.ru>
2025-08-06 17:00:38 +0300

    API test V2

--- data/data.py
+++ data/data.py
@@ -1,3 +1,13 @@
+from faker import Faker
+import random
+
+
+fake = Faker()
+
+data_user = {'email': fake.email(),
+             'password': fake.password() + str(random.choice(range(1, 100000))),
+             'name': fake.name() + str(random.choice(range(1, 100000)))}
+
 BASE_URL = 'https://stellarburgers.nomoreparties.site/'
 CREATING_UNIQUE_USER_POST = BASE_URL + 'api/auth/register'
 LOGIN_USER_POST = BASE_URL + 'api/auth/login'
@@ -8,6 +18,8 @@ MESSAGE_USER_EXISTS = "User already exists"
 MESSAGE_REQUIRED_FIELD = "Email, password and name are required fields"
 MESSAGE_INCORRECT_LOGIN = "email or password are incorrect"
 MESSAGE_NOT_INGREDIENT = "Ingredient ids must be provided"
+MESSAGE_NOT_AUTH = "You should be authorised"
+
 
 list_param_reg = [
     (None, 'some99password56', 'some465first55Name89'),
@@ -21,4 +33,4 @@ INGREDIENTS = [
     "61c0c5a71d1f82001bdaaa71"
 ]
 
-BAD_INGREDIENTS = [None, ("82001b", "c5a71")]
+BAD_INGREDIENTS = ("82001b", "c5a71")
--- test/test_order.py
+++ test/test_order.py
@@ -1,7 +1,6 @@
-import pytest
 import allure
 from methods.request import create_order
-from data.data import INGREDIENTS, BAD_INGREDIENTS, MESSAGE_NOT_INGREDIENT
+from data.data import INGREDIENTS, BAD_INGREDIENTS, MESSAGE_NOT_INGREDIENT, MESSAGE_NOT_AUTH
 
 
 @allure.epic('Класс тестирования заказов')
@@ -22,19 +21,24 @@ class TestOrder:
     @allure.title('Тестирование невозможности создания заказа неавторизованным пользователем')
     def test_order_without_auth_negative(self):
         with allure.step('отправка запроса'):
-            _, status_code = create_order(ingredients=INGREDIENTS, token=None)
-        with allure.step('Проверка невозможности создания заказа'):
-            assert status_code != 200, 'Допускается создание заказа без авторизации'
+            response, status_code = create_order(ingredients=INGREDIENTS, token=None)
+        with (allure.step('Проверка невозможности создания заказа')):
+            assert status_code == 401 and (response.json()).get("message") == MESSAGE_NOT_AUTH
 
     @allure.title('Тестирование невозможности создания заказа с невалидными данными')
-    @pytest.mark.parametrize('ingr', BAD_INGREDIENTS)
-    def test_order_with_bad_ingredient_negative(self, reg_log_del_user, ingr):
+    def test_order_with_bad_ingredient_negative(self, reg_log_del_user):
         with allure.step('Подготовка тестовых данных и отправка запроса'):
             token = reg_log_del_user
-            response, status_code = create_order(ingredients=ingr, token=token)
+            _, status_code = create_order(ingredients=BAD_INGREDIENTS, token=token)
         with allure.step('Проверка невозможности создания заказа с невалидными данными'):
-            if not ingr:
-                response = response.json()
-                assert status_code == 400 and response["message"] == MESSAGE_NOT_INGREDIENT
-            else:
-                assert status_code == 500
+            # нет тело ответа сервера, возвращает только статус код
+            assert status_code == 500
+
+    @allure.title('Тестирование невозможности создания заказа без ингридиентов')
+    def test_order_without_ingredient_negative(self, reg_log_del_user):
+        with allure.step('Подготовка тестовых данных и отправка запроса'):
+            token = reg_log_del_user
+            response, status_code = create_order(ingredients=None, token=token)
+        with allure.step('Проверка невозможности создания заказа с невалидными данными'):
+            response = response.json()
+            assert status_code == 400 and response["message"] == MESSAGE_NOT_INGREDIENT
